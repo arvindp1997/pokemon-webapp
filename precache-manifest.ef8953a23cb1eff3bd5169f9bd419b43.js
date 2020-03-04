4a1206a10a844e3b2d481dd91fd5f48a84bf9f02
ARAVIND <arvindputtu@gmail.com>
2020-03-05 04:36:29 +0530

    Updates

---
+++ precache-manifest.ef8953a23cb1eff3bd5169f9bd419b43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91e575b7888096b2f9969c1d06138cb",
+    "url": "/pokemon-webapp/index.html"
+  },
+  {
+    "revision": "363360fa744f4f242ffe",
+    "url": "/pokemon-webapp/static/css/main.1d8e237d.chunk.css"
+  },
+  {
+    "revision": "3be23dd4976434f2aa11",
+    "url": "/pokemon-webapp/static/js/2.bd363598.chunk.js"
+  },
+  {
+    "revision": "363360fa744f4f242ffe",
+    "url": "/pokemon-webapp/static/js/main.611498dc.chunk.js"
+  },
+  {
+    "revision": "12971596fcfd59b5aa20",
+    "url": "/pokemon-webapp/static/js/runtime~main.985eb78e.js"
+  }
+]);
